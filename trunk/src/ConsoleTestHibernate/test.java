0f05581098aa3111c9636b5c6e31cf85f8b402c0
bontru2@gmail.com <bontru2@gmail.com@adeb4819-5ba8-0442-725d-8be479702fb1>
2013-06-04 08:56:23 +0000

    comit code

---
+++ trunk/src/ConsoleTestHibernate/test.java
@@ -0,0 +1,12 @@
+package ConsoleTestHibernate;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 
+	}
+
+}
---
+++ trunk/src/bussiness/entity/Custommers.java
@@ -0,0 +1,81 @@
+package bussiness.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="custommers")
+public class Custommers {
+
+	@Id
+	@GeneratedValue
+	int id;
+	
+	@Column(name="fullName")
+	String fullName; 
+	
+	@Column(name="address")
+	String address; 
+	
+	@Column(name="telephone")
+	String telephone; 
+	
+	@Column(name="email")
+	String email; 
+	
+	@Column(name="active")
+	boolean active;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+}
