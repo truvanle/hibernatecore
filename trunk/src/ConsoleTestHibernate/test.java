f26d8db8b97bb466874436067bb9c2dc4a1bb6b7
bontru2@gmail.com <bontru2@gmail.com@adeb4819-5ba8-0442-725d-8be479702fb1>
2013-06-05 08:55:36 +0000

    test hibernate

--- trunk/src/ConsoleTestHibernate/test.java
+++ trunk/src/ConsoleTestHibernate/test.java
@@ -1,12 +1,38 @@
 package ConsoleTestHibernate;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.sql.ordering.antlr.Factory;
+
+import bussiness.entity.Custommers;
+
 public class test {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 
+		Configuration config = new AnnotationConfiguration().configure();
+		SessionFactory factory = config.buildSessionFactory();
+		Session session = factory.openSession();
+		
+		String hql = "FROM Custommers";
+		Query query = session.createQuery(hql);
+		
+		List<Custommers> entities = query.list();
+		
+		for(Custommers o : entities){
+			System.out.println(" >> ID: " + o.getId());
+			System.out.println(" >> FullName: " + o.getFullName());
+			System.out.println();
+		}
+		
+		session.close();
 	}
 
 }
